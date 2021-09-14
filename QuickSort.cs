1d303ecabf9730ec387505251e6efbaf210ee854
Максим Галанов <mgalanov404@mso365.technolog.edu.ru>
2021-09-14 14:49:45 +0300

    Готовая быстрая сортировка

--- QuickSort.cs
+++ QuickSort.cs
@@ -36,24 +36,51 @@ namespace Quick
             return pivot;
         }
 
+        int PartitionDescending(List<int> array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                this.comparison++;
+                if (array[i] > array[maxIndex])
+                {
+                    pivot++;
+
+                    Swap(ref array, pivot, i);
+                }
+            }
+
+            pivot++;
+            Swap(ref array, pivot, maxIndex);
+            return pivot;
+        }
+
         //быстрая сортировка
-        List<int> Quick_Sort(List<int> array, int minIndex, int maxIndex)
+        List<int> Quick_Sort(List<int> array, int minIndex, int maxIndex, bool isAscending)
         {
             if (minIndex >= maxIndex)
             {
                 return array;
             }
-
-            var pivotIndex = Partition(array, minIndex, maxIndex);
-            Quick_Sort(array, minIndex, pivotIndex - 1);
-            Quick_Sort(array, pivotIndex + 1, maxIndex);
+            if (isAscending)
+            {
+                var pivotIndex = Partition(array, minIndex, maxIndex);
+                Quick_Sort(array, minIndex, pivotIndex - 1, isAscending);
+                Quick_Sort(array, pivotIndex + 1, maxIndex, isAscending);
+            }
+            else {
+                var pivotIndex = PartitionDescending(array, minIndex, maxIndex);
+                Quick_Sort(array, minIndex, pivotIndex - 1, isAscending);
+                Quick_Sort(array, pivotIndex + 1, maxIndex, isAscending);
+            }
+            
 
             return array;
         }
         public override List<int> sort(List<int> arr, bool isAscending)
         {
 
-            Quick_Sort(arr, 0, arr.Count - 1);
+            Quick_Sort(arr, 0, arr.Count - 1, isAscending);
 
             return arr;
 
